b7ad749b3a50aa0188efaff9ede44b91a347dac8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-03-07 08:10:46 +0900

    Update eslint config for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,42 @@
+const extentions = ['.ts', '.tsx', '.js', '.jsx', '.vue', '.json', '.node'];
+
 module.exports = {
-  extends: [
-    'eslint-config-xo-space/esnext',
-    'eslint-config-prettier',
-    'eslint-config-prettier/standard',
-  ],
   env: {
     node: true,
+    browser: true,
+    commonjs: true,
+    serviceworker: true,
+  },
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+    useJSXTextNode: true,
+    extraFileExtensions: extentions,
+  },
+  settings: {
+    node: {
+      tryExtensions: extentions,
+    },
   },
   rules: {
-    'comma-dangle': [
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': 'error',
+
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    'node/no-missing-import': [
       'error',
-      'always-multiline',
+      {
+        allowModules: [],
+        resolvePaths: ['./src'],
+        tryExtensions: extentions,
+      },
     ],
-  }
+    'node/no-unsupported-features/es-syntax': 'off',
+  },
 };
